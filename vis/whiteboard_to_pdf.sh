14fba54ec8a34d386b70d6a94dcaa7ea1f8be68d
Jack Roberts <jroberts@turing.ac.uk>
2019-04-18 13:52:10 +0100

    script to convert html table into single page pdf

---
+++ vis/whiteboard_to_pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ../data/figs/projects
+wkhtmltopdf -T 0 -B 0 --page-width 2000mm --page-height 2000mm projects.html tmp.pdf
+pdf-crop-margins -p 0 -a -10 tmp.pdf -o projects.pdf
+rm tmp.pdf
+
+cd ../people
+wkhtmltopdf -T 0 -B 0 --page-width 2000mm --page-height 2000mm people.html tmp.pdf
+pdf-crop-margins -p 0 -a -10 tmp.pdf -o people.pdf
+rm tmp.pdf
