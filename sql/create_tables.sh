d2f4141a9c5f84e98dc822ef09fec51bf9ccc20d
Jack Roberts <jroberts@turing.ac.uk>
2019-04-29 15:29:08 +0100

    Delete create_tables.sh

--- sql/create_tables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export PGPASSWORD=Wf80GBRsZv9HjndH5yYdXox3vmIfkcWa
-
-createdb --host=wimbledon-planner.postgres.database.azure.com --port=5432 --username=jack@wimbledon-planner wimbledon
-
-psql --host=wimbledon-planner.postgres.database.azure.com --port=5432 --username=jack@wimbledon-planner --dbname=wimbledon -f harvest.sql
-
-psql --host=wimbledon-planner.postgres.database.azure.com --port=5432 --username=jack@wimbledon-planner --dbname=wimbledon -f forecast.sql
