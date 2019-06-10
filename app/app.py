1418b1e296a75b95202a6b0b21d308f1a529704a
Jack Roberts <jroberts@turing.ac.uk>
2019-06-10 11:34:33 +0100

    try except on download code

--- app/app.py
+++ app/app.py
@@ -69,15 +69,19 @@ def projects():
 
 @app.route('/download')
 def download():
-    subprocess.call(["sh", "../scripts/whiteboard_to_pdf.sh"])
+    try:
+        subprocess.call(["sh", "../scripts/whiteboard_to_pdf.sh"])
 
-    with zipfile.ZipFile(DATA_DIR+'/whiteboard.zip', 'w') as zipf:
-        zipf.write(DATA_DIR+'/figs/projects/projects.html', 'projects.html')
-        zipf.write(DATA_DIR+'/figs/people/people.html', 'people.html')
-        zipf.write(DATA_DIR + '/figs/projects/projects.pdf', 'projects.pdf')
-        zipf.write(DATA_DIR + '/figs/people/people.pdf', 'people.pdf')
+        with zipfile.ZipFile(DATA_DIR+'/whiteboard.zip', 'w') as zipf:
+            zipf.write(DATA_DIR+'/figs/projects/projects.html', 'projects.html')
+            zipf.write(DATA_DIR+'/figs/people/people.html', 'people.html')
+            zipf.write(DATA_DIR + '/figs/projects/projects.pdf', 'projects.pdf')
+            zipf.write(DATA_DIR + '/figs/people/people.pdf', 'people.pdf')
 
-    return send_from_directory(DATA_DIR, 'whiteboard.zip')
+        return send_from_directory(DATA_DIR, 'whiteboard.zip')
+
+    except:
+        return traceback.format_exc()
 
 
 @app.route('/people')
